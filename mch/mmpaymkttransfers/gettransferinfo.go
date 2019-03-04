da136cf891e274d1c93623e65adde76b735522fa
Sam.Huang <531049775@qq.com>
2019-03-04 14:28:53 +0800

    提交sha1.New()

--- mch/mmpaymkttransfers/gettransferinfo.go
+++ mch/mmpaymkttransfers/gettransferinfo.go
@@ -4,8 +4,18 @@ import (
 	"github.com/samhuangszu/wechat/mch/core"
 )
 
-// 查询企业付款.
+//GetTransferInfo 查询企业付款.
 //  NOTE: 请求需要双向证书
 func GetTransferInfo(clt *core.Client, req map[string]string) (resp map[string]string, err error) {
 	return clt.PostXML(core.APIBaseURL()+"/mmpaymkttransfers/gettransferinfo", req)
+}
+
+// TransferInfoReq 查询付款到银行卡
+type TransferInfoReq struct {
+	XMLName        struct{} `xml:"xml" json:"-"`
+	MchID          string   `xml:"mch_id" json:"mch_id"`
+	AppID          string   `xml:"appid" json:"appid"`
+	PartnerTradeNo string   `xml:"partner_trade_no" json:"partner_trade_no"`
+	NonceStr       string   `xml:"nonce_str" json:"nonce_str"`
+	Sign           string   `xml:"sign" json:"sign"`
 }
--- mch/mmpaymkttransfers/promotion/transfers.go
+++ mch/mmpaymkttransfers/promotion/transfers.go
@@ -4,8 +4,23 @@ import (
 	"github.com/samhuangszu/wechat/mch/core"
 )
 
-// 企业付款.
+//Transfers 企业付款.
 //  NOTE: 请求需要双向证书
 func Transfers(clt *core.Client, req map[string]string) (resp map[string]string, err error) {
 	return clt.PostXML(core.APIBaseURL()+"/mmpaymkttransfers/promotion/transfers", req)
+}
+
+// Req 付款到零钱
+type Req struct {
+	XMLName        struct{} `xml:"xml" json:"-"`
+	MchAppID       string   `xml:"mch_appid" json:"mch_appid"`
+	MchID          string   `xml:"mch_id" json:"mch_id"`
+	NonceStr       string   `xml:"nonce_str" json:"nonce_str"`
+	Sign           string   `xml:"sign" json:"sign"`
+	PartnerTradeNo string   `xml:"partner_trade_no" json:"partner_trade_no"`
+	OpenID         string   `xml:"openid" json:"openid"`
+	CheckName      string   `xml:"check_name" json:"check_name"`
+	Amount         int      `xml:"amount" json:"amount,string"`
+	Desc           string   `xml:"desc" json:"desc"`
+	SpbillCreateIP string   `xml:"spbill_create_ip" json:"spbill_create_ip"`
 }
